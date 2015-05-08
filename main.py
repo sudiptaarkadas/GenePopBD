12a25f1b1a6669c8447b8470613d06d203873be2
sudiptaarkadas <sudipta.arka.das@gmail.com>
2015-05-09 03:44:29 +0600

    Update main.py
    Added,
    Matching Probability
    Power of Discrimination
    PIC

--- main.py
+++ main.py
@@ -12,12 +12,14 @@ data = csv_file.load()
 for j in range(1, len(data[0])-1,2):
   allele_l = []
   allele_r = []
+  allele_g = []
 
   for i in range(1,len(data)):
     allele_l.append(data[i][j])
     allele_r.append(data[i][j+1])
+    allele_g.append(data[i][j]+','+data[i][j+1])
 
-  forensic = Forensic(allele_l, allele_r)
+  forensic = Forensic(allele_l, allele_r, allele_g)
 
   per_homo_hetero = forensic.per_homo_hetero()
   
@@ -42,12 +44,27 @@ for j in range(1, len(data[0])-1,2):
   print '\n \t Frequency Calculations'
   print '\t --------------------------'
   print '\t Total allele = ', freq_calc[0]
+  print '\t PIC = ', freq_calc[6]
   print '\t Table for frequency '
   print '\t --------------------------'
-  print '\n \t Allele \t Frequency \t Percent'
+  print '\n \t Allele \t Frequency \t Percent \t Pi2 \t Pi4'
 
   for k in range(len(freq_calc[1])):
-    print '\t',freq_calc[1][k], '\t', freq_calc[2][k], '\t', freq_calc[3][k]
+    print '\t',freq_calc[1][k], '\t', freq_calc[2][k], '\t', freq_calc[3][k], '\t', freq_calc[4][k], '\t', freq_calc[5][k]
+
+
+  geno_calc = forensic.geno_calc()
+  
+  print '\n \t Genotype Calculations'
+  print '\t --------------------------'
+  print '\t Total Genotypes = ', geno_calc[0]
+  print '\t Matching Probability = ', geno_calc[4]
+  print '\t Power of Discrimination = ', geno_calc[5]
+  print '\t Table for frequency '
+  print '\t --------------------------'
+  print '\n \t Genotype \t Frequency \t Percent'
+
+  for k in range(len(geno_calc[1])):
+    print '\t',geno_calc[1][k], '\t', geno_calc[2][k], '\t', geno_calc[3][k]
 
   'print (j+1)/2, forensic.per_homo_hetero(), forensic.foren_param(), forensic.freq_calc()'
-
